c727d7811d9666141452889e5eb89771eaec2a2e
VatiMorales <vtmorales@misena.edu.co>
2021-03-21 20:29:45 -0500

    Se configuro la base de datos en el Web.config,En la carpeta Models se añadio la clase MantenimeintoUsuario, En el controlador se añadio el HomeController. Cambio de inicial Mayuscula en SE_Usuarios.

--- Models/SE_Usuarios.cs
+++ Models/SE_Usuarios.cs
@@ -7,22 +7,22 @@ namespace ProgramaEgresadoSena.Models
 {
     public class SE_Usuarios
     {
-        public int usu_id { get; set; }
-        public int usu_documento { get; set; }
-        public string usu_tipodoc { get; set; }
-        public string usu_nombre { get; set; }
-        public int usu_celular { get; set; }
-        public string usu_email { get; set; }
-        public string usu_genero{ get; set; }
-        public bool usu_aprendiz { get; set; }
-        public bool usu_egresado { get; set; }
-        public string usu_areaformacion { get; set; }
-        public DateTime usu_fechaegresado { get; set; }
-        public string usu_direccion { get; set; }
-        public string usu_barrio { get; set; }
-        public string usu_ciudad { get; set; }
-        public string usu_departamento { get; set; }
-        public DateTime usu_fecharegistro { get; set; }
+        public int Usu_id { get; set; }
+        public int Usu_documento { get; set; }
+        public string Usu_tipodoc { get; set; }
+        public string Usu_nombre { get; set; }
+        public int Usu_celular { get; set; }
+        public string Usu_email { get; set; }
+        public string Usu_genero{ get; set; }
+        public bool Usu_aprendiz { get; set; }
+        public bool Usu_egresado { get; set; }
+        public string Usu_areaformacion { get; set; }
+        public DateTime Usu_fechaegresado { get; set; }
+        public string Usu_direccion { get; set; }
+        public string Usu_barrio { get; set; }
+        public string Usu_ciudad { get; set; }
+        public string Usu_departamento { get; set; }
+        public DateTime Usu_fecharegistro { get; set; }
 
     }
 }
